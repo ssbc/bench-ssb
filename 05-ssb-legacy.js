1392acc9ce2921ba059ef36daf0edab114bfa376
Dominic Tarr <dominic.tarr@gmail.com>
2017-10-27 22:51:26 +1300

    ensure ssb keys follow the generated identities!

--- 05-ssb-legacy.js
+++ 05-ssb-legacy.js
@@ -1,17 +1,28 @@
 var SSB = require('secure-scuttlebutt')
 var data = require('./output.json')
-
+var ssbKeys = require('ssb-keys')
+var path = require('path')
 var rmrf = require('rimraf')
 
 var dir = '/tmp/bench-ssb-legacy_ssb/'
 rmrf.sync(dir)
 
+var keys = ssbKeys.loadOrCreateSync(path.join(dir, 'secret'))
+
 var db = SSB(null, {}, null, dir)
 db.ready.set(true)
+
+var feed = db.createFeed(keys)
 
 var log = require('./util')('legacy')
 
 var pull = require('pull-stream')
+
+feed.publish({
+  type: 'contact',
+  contact: data.queue[0].author,
+  following: true
+}, function () {})
 
 pull(
   pull.values(data.queue),
