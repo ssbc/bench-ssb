465fbb6317cef80d79a35df4d9505c80443c3af7
robert <robert.franklin.iv@gmail.com>
2018-01-09 20:23:22 -0600

    Updated the display functionality in util.js to clarify the output.

--- util.js
+++ util.js
@@ -1,17 +1,69 @@
+var readline = require('readline');
+var Table = require('cli-table')
 
 module.exports = function (name) {
   var start
-  var ts = start = Date.now()
-  var total = 0, _total = 0
+  var lastLogTime = start = Date.now()
+  var runCount = 0, previousRunCount = 0
+  var table = createTable()  
+  var nextStatus = getStatusFn()
+  nextStatus()
+
   return function (number, end) {
-    total += number
-    var _ts = Date.now()
-    if(end || _ts > ts + 1000) {
-      ts = _ts
-      var seconds = (_ts-start)/1000
-      console.log(name, total, total-_total, total/seconds, seconds)
-      _total = total
+    runCount += number
+    var currentTime = Date.now()
+
+    if(end || currentTime > lastLogTime + 1000) {
+      lastLogTime = currentTime
+      var seconds = (currentTime-start)/1000
+      table.push([
+        seconds.toFixed(1),
+        runCount,
+        runCount-previousRunCount,
+        (runCount/seconds).toFixed()
+      ])
+      nextStatus()
+      previousRunCount = runCount
+    }
+
+    if(end) {
+      clearLine()
+      console.log(table.toString())
+      var avgRate = runCount / seconds
+      logBright(`Overall average execution rate: ${avgRate.toFixed()} per second`)
     }
   }
 }
 
+function createTable() {
+  return new Table({
+    head: [
+           'Time (s)',
+           '# Executions (total)', 
+           '# Executions (per row)', 
+           'Average Rate (#/s)',
+          ]
+  })
+}
+
+function getStatusFn() {
+  var spinner = ['|', '/', '-', '\\']
+  var ellipses = ['.', '..', '...']
+  var i = 0
+
+  return () => {
+    clearLine()
+    process.stdout.write(spinner[i % spinner.length] + ' Executing benchmark' + ellipses[i % ellipses.length])
+    i++
+  };
+}
+
+function clearLine() {
+  process.stdout.clearLine()
+  process.stdout.cursorTo(0)
+}
+
+function logBright(...params) {
+  console.log('\x1b[1m', ...params)
+  console.log('\x1b[0m', '') // Resets the color to normal.
+}
