aa3f13e02ab33eb4216fdabc1ed2dc45a5c1791f
Anders Rune Jensen <arj03@protonmail.ch>
2018-05-25 00:28:25 +0200

    Actually test the performance of json and not array shift

--- 02-json.js
+++ 02-json.js
@@ -1,19 +1,15 @@
-
 var _state = require('./output.json')
 
-var V = require('ssb-validate')
 var U = require('./util')
 var queue = _state.queue
 var log = U('json')
-var state = V.initial()
 var i=0
 
-while(queue.length) {
-//  state = V.append(state, null, queue.shift())
-  var msg = queue.shift()
-  JSON.stringify(msg, null, 2)
-  log(1, ++i % 10000 == 0)
+for (let i = 0; i < queue.length; ++i) {
+  JSON.stringify(queue[i], null, 2)
+  log(1, i % 10000 == 0)
 }
+
 log(null, true)
 
 
