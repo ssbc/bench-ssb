210053c43bc2640c24f052a669b8376e2335fb04
Dominic Tarr <dominic.tarr@gmail.com>
2018-10-28 08:20:43 +0530

    better run-all script

--- run-all.sh
+++ run-all.sh
@@ -1,23 +1,11 @@
-node 00-generate.js
-echo "running 01"
-node 01-validate.js > 01-out.txt
-echo "running 02"
-node 02-json.js > 02-out.txt
-echo "running 03"
-node 03-flume.js > 03-out.txt
-echo "running 04"
-node 04-minimal.js > 04-out.txt
-echo "running 05"
-node 05-ssb-legacy.js > 05-out.txt
-echo "running 06"
-node 06-ssb-read.js > 06-out.txt
-echo "running 07"
-node 07-clock-dump.js > 07-out.txt
-echo "running 08"
-node 08-sbot-read.js > 08-out.txt
-echo "running 09"
-node 09-sbot-replicate.js > 09-out.txt
-echo "running 10"
-node 10-sbot-replicate-post.js > 10-out.txt
-echo "running 11"
-node 11-sbot-replicate-ebt.js > 11-out.txt
+run=$1
+rm -rf data/"$run"
+mkdir -p data/"$run"
+{
+  echo 'name, total, sample, average, seconds'
+  for bench in {0,1}*.js; do
+    node $bench > data/"$run"/"$bench".csv
+    tail -n 1 data/"$run"/"$bench".csv
+  done
+} | tee data/"$run"/summary.csv
+
