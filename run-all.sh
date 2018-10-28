ce9780a2e6b5fe7a857aa1ba30f084d13eef8edb
Dominic Tarr <dominic.tarr@gmail.com>
2018-10-28 13:59:23 +0530

    update run-all script

--- run-all.sh
+++ run-all.sh
@@ -1,11 +1,26 @@
 run=$1
-rm -rf data/"$run"
-mkdir -p data/"$run"
+
+if [ x$run = x ]; then
+  run=$(node -p 'new Date()')
+fi
+
+mkdir -p data/working
+rm -rf data/working/*
+
+echo benchmark run: $run
+
 {
-  echo 'name, total, sample, average, seconds'
+  echo 'name, seconds, total, total/seconds'
   for bench in {0,1}*.js; do
-    node $bench > data/"$run"/"$bench".csv
-    tail -n 1 data/"$run"/"$bench".csv
+    node $bench > data/working/"$bench".csv
+    tail -n 1 data/working/"$bench".csv
   done
-} | tee data/"$run"/summary.csv
+} | tee data/working/summary.csv
+
+svg-graph name,seconds < data/working/summary.csv > data/working/chart.svg
+
+mv data/working data/"$run"
+rm data/latest
+
+ln -s "$run" data/latest
 
